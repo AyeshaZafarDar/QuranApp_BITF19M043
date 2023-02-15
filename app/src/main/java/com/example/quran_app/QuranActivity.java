2997403a81a814a23d332f75ff9009810b2c7337
bitf19m043@pucit.edu.pk <bitf19m043@pucit.edu.pk>
2023-02-16 00:24:41 +0500

    Repository button added

--- app/src/main/java/com/example/quran_app/QuranActivity.java
+++ app/src/main/java/com/example/quran_app/QuranActivity.java
@@ -2,6 +2,8 @@ package com.example.quran_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,7 @@ public class QuranActivity extends AppCompatActivity implements View.OnClickList
     public Button btnext;
     public Button btpre;
     public Button btsearch;
+    public Button Repobtn;
 
 //Edit text views for surat and ayat number
     public EditText txts;
@@ -41,10 +44,12 @@ public class QuranActivity extends AppCompatActivity implements View.OnClickList
         txtresult = findViewById(R.id.result);
         txta = findViewById(R.id.txt_a);
         txts = findViewById(R.id.txt_s);
+        Repobtn = findViewById(R.id.rpbutton);
 
         btsearch.setOnClickListener(this);
         btnext.setOnClickListener(this);
         btpre.setOnClickListener(this);
+        Repobtn.setOnClickListener(this);
 
 
         theAyat = new QDH();
@@ -79,6 +84,13 @@ public class QuranActivity extends AppCompatActivity implements View.OnClickList
                 }
                 AYAT = data.QuranArabicText[Ayat_num];
                 txtresult.setText(AYAT.toString());
+                break;
+
+            case R.id.rpbutton:
+                String url = "https://github.com/AyeshaZafarDar/QuranApp_BITF19M043";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
                 break;
 
             default:
